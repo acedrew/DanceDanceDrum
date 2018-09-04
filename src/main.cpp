4f30e75cda8dd57a7425958dc3b46896ace5b49e
Andrew Rodgers <acedrew@gmail.com>
2018-09-04 09:44:42 -0400

    Update after crazy weekend
    Added lights, naive light changes implemented..
    Currently there's an issue with the hardware preventing the latter half of the LEDs from operating, may be a bad   led module in the strip?

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <main.h>
+#include <WS2812B.h>
 
 #define DRUM1 PB1
 #define DRUM2 PB0
@@ -7,14 +8,30 @@
 #define DRUM4 PA6
 #define DRUM5 PA5
 
-#define PRINT_INTERVAL 1000
+#define PRINT_INTERVAL 250
+#define SHOW_INTERVAL 20
+
+#define NUM_LEDS 100
 
 bool drumState[5] = {false};
 bool not_scanned = true;
 int drumVals[5] = {0};
 int drums[5] = {DRUM1, DRUM2, DRUM3, DRUM4, DRUM5};
 int drumThresholds[5] = {800, 1200, 1000, 1000, 1000};
+
+uint32_t drumColors[5] = {
+    255 << 16,
+    255 << 8,
+    255,
+    (128 << 16) + 128,
+    (128 << 8) + 128
+};
+uint32_t currentColor = 0;
+//loop timer state
 int lastLoop = 0;
+int showLoop = 0;
+
+WS2812B strip = WS2812B(NUM_LEDS);
 
 
 
@@ -29,8 +46,8 @@ void setup() {
     // attachInterrupt(DRUM1, checkDrums, RISING);
     // attachInterrupt(DRUM2, checkDrums, RISING);
     // attachInterrupt(DRUM3, checkDrums, RISING);
-    // attachInterrupt(DRUM4, checkDrums, RISING);
-    // attachInterrupt(DRUM5, checkDrums, RISING);
+    // attachInterrupt(DRUM4, checkDrums, RISING); // attachInterrupt(DRUM5, checkDrums, RISING);
+    strip.begin();
     Serial.begin(115200);
 
 }
@@ -43,6 +60,19 @@ void loop() {
     } else {
         not_scanned = true;
     }
+    if (loopMillis >= showLoop + SHOW_INTERVAL) {
+        showLoop = loopMillis;
+        for(int i = 0; i < 5; i++) {
+            if (drumState[i]) {
+                currentColor = drumColors[i];
+            }
+        }
+        for(int i = 0; i < NUM_LEDS; i++) {
+                strip.setPixelColor(i, currentColor); 
+        }
+        strip.show();
+        currentColor = 0;
+    }
 
     if(loopMillis - lastLoop > PRINT_INTERVAL){
         lastLoop = loopMillis;
@@ -52,10 +82,9 @@ void loop() {
             drumState[i] = false;
         }
         Serial.print('\n');
+        Serial.println(currentColor);
         checkDrums();
-
     }
-
     // put your main code here, to run repeatedly:
 }
 
