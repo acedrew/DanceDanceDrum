0ba44846c8b0b981da0784f79c22b442b8639305
Andrew Rodgers <acedrew@gmail.com>
2018-09-17 15:43:31 -0400

    Added pump read, fixed thresholds

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <libmaple/usb_cdcacm.h>
 #include <main.h>
 #include <WS2812B.h>
 
@@ -8,23 +9,29 @@
 #define DRUM4 PA6
 #define DRUM5 PA5
 
+#define PUMP PA0
+
 #define PRINT_INTERVAL 250
 #define SHOW_INTERVAL 20
 
 #define NUM_LEDS 100
 
+#define PUMPMAX 3200
+#define PUMPMIN 1000
+
 bool drumState[5] = {false};
-bool not_scanned = true;
+bool not_scanned = true; 
 int drumVals[5] = {0};
 int drums[5] = {DRUM1, DRUM2, DRUM3, DRUM4, DRUM5};
-int drumThresholds[5] = {800, 1200, 1000, 1000, 1000};
+int drumThresholds[5] = {1500, 1300, 1310, 1300, 1300};
+int drumTimes[5] = {0};
 
 uint32_t drumColors[5] = {
-    255 << 16,
-    255 << 8,
-    255,
-    (128 << 16) + 128,
-    (128 << 8) + 128
+    0xfe0000,
+    0xf9fe00,
+    0xfe3000,
+    0x0003fe,
+    0x9d00fe
 };
 uint32_t currentColor = 0;
 //loop timer state
@@ -33,6 +40,18 @@ int showLoop = 0;
 
 WS2812B strip = WS2812B(NUM_LEDS);
 
+static const usb_descriptor_string usbVcomDescriptor_iManufacturer = {
+    .bLength = USB_DESCRIPTOR_STRING_LEN(8),
+    .bDescriptorType = USB_DESCRIPTOR_TYPE_STRING,
+    .bString = {'L', 0, 'e', 0, 'a', 0, 'f', 0,
+                'L', 0, 'a', 0, 'b', 0, 's', 0},
+};
+
+static const usb_descriptor_string usbVcomDescriptor_iProduct = {
+    .bLength = USB_DESCRIPTOR_STRING_LEN(5),
+    .bDescriptorType = USB_DESCRIPTOR_TYPE_STRING,
+    .bString = {'D', 0, 'r', 0, 'u', 0, 'm', 0, 's', 0},
+};
 
 
 void setup() {
@@ -53,13 +72,8 @@ void setup() {
 }
 
 void loop() {
+    checkDrums();
     int loopMillis = millis();
-    if (loopMillis % 10 == 0 && not_scanned) {
-        checkDrums();
-        not_scanned = false;
-    } else {
-        not_scanned = true;
-    }
     if (loopMillis >= showLoop + SHOW_INTERVAL) {
         showLoop = loopMillis;
         for(int i = 0; i < 5; i++) {
@@ -76,21 +90,32 @@ void loop() {
 
     if(loopMillis - lastLoop > PRINT_INTERVAL){
         lastLoop = loopMillis;
-        for(int i = 0; i<5; i++) {
-            Serial.print(drumState[i]);
-            Serial.print(' ');
-            drumState[i] = false;
-        }
-        Serial.print('\n');
-        Serial.println(currentColor);
-        checkDrums();
+        Serial.write(4);
+        Serial.flush();
+        Serial.write(constrain(map(analogRead(PUMP), PUMPMIN, PUMPMAX, 255, 0), 0, 255));
+        Serial.flush();
     }
-    // put your main code here, to run repeatedly:
 }
 
 void checkDrums() {
+    int drumMillis = millis();
     for(int i=0; i<5; i++) {
-        drumState[i] = (analogRead(drums[i]) > drumThresholds[i]) || drumState[i];
+        int currentDrumVal = analogRead(drums[i]);
+        if (drumMillis > drumTimes[i] + 200) {
+            if (currentDrumVal > drumThresholds[i]) {
+                // Serial.println(currentDrumVal);
+                // Serial.println(drumThresholds[i]);
+                // Serial.println(i);
+                drumState[i] = true;
+                drumTimes[i] = drumMillis;
+                Serial.write(2);
+                Serial.flush();
+                Serial.write(i);
+                Serial.flush();
+            } else {
+                drumState[i] =  false;
+            }
+        }
     }
     
 }
